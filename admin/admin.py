5e46b12e3c4aac73758876f586337cc2c1f4c3ac
Socheapheaktra <89124423+Socheapheaktra@users.noreply.github.com>
2022-06-20 17:01:21 +0700

    Completed Some More Functions in Admin.py

--- admin/admin.py
+++ admin/admin.py
@@ -35,6 +35,7 @@ class AdminWindow(MDBoxLayout):
         self.mycursor = self.mydb.cursor()
 
         self.dialog = None
+        self.dob_date = None
 
         self.show_user_table()
 
@@ -182,6 +183,18 @@ class AdminWindow(MDBoxLayout):
         else:
             pass
 
+    #FIXME
+    def add_user(self):
+        pass
+
+    #FIXME
+    def update_user(self):
+        pass
+
+    #FIXME
+    def remove_user(self):
+        pass
+
     def add_bus(self, name, price, bus_type):
         bus_type_list = ["Express", "VIP"]
         if name == "" or price == "":
@@ -299,28 +312,172 @@ class AdminWindow(MDBoxLayout):
                 )
                 self.dialog.open()
 
+    #FIXME
     def add_trip(self, location, price):
         pass
 
+    #FIXME
     def update_trip(self):
         pass
 
+    #FIXME (DONE)
     def update_password(self, old_pass, new_pass, confirm_pass):
-        pass
+        sql = 'SELECT user_pass FROM users WHERE user_name=%s'
+        values = [self.ids.nav_drawer_header.text, ]
+        self.mycursor.execute(sql, values)
+        result = self.mycursor.fetchall()
+        u_pass = result[0][0]
+        print(u_pass)
+        if old_pass == "" or new_pass == "" or confirm_pass == "":
+            self.dialog = MDDialog(
+                title="All Fields Required!",
+                text="Please fill in all fields to continue",
+                buttons=[
+                    MDFlatButton(
+                        text="Close",
+                        on_release=self.close_dialog
+                    )
+                ]
+            )
+            self.dialog.open()
+        else:
+            if old_pass != u_pass:
+                self.ids.old_pass.error = True
+            else:
+                if new_pass != confirm_pass:
+                    self.ids.confirm_pass.error = True
+                else:
+                    try:
+                        sql = 'UPDATE users SET user_pass = %s WHERE user_name =%s'
+                        values = [new_pass, self.ids.nav_drawer_header.text, ]
+                        self.mycursor.execute(sql, values)
+                        self.mydb.commit()
+                    except:
+                        self.dialog = MDDialog(
+                            title="Error!",
+                            text="Cannot Change Password!",
+                            buttons=[
+                                MDFlatButton(
+                                    text="Close",
+                                    on_release=self.close_dialog
+                                )
+                            ]
+                        )
+                        self.dialog.open()
+                    else:
+                        self.dialog = MDDialog(
+                            title="Success!",
+                            text="Your Password has been updated!",
+                            buttons=[
+                                MDFlatButton(
+                                    text="Close",
+                                    on_release=self.close_dialog
+                                )
+                            ]
+                        )
+                        self.dialog.open()
 
+    #FIXME (DONE)
     def update_info(self, first_name, last_name, phone, email, dob):
-        pass
+        if first_name == "" or last_name == "":
+            f_name = None
+            l_name = None
+        else:
+            f_name = first_name
+            l_name = last_name
+        if phone == "":
+            phone_num = None
+        else:
+            phone_num = phone
+
+        if dob == "":
+            u_dob = None
+        else:
+            u_dob = dob
+        if email == "":
+            self.ids.email_fld.error = True
+        else:
+            email_addr = email
+            try:
+                sql = 'UPDATE users SET ' \
+                      'first_name = %s, ' \
+                      'last_name = %s, ' \
+                      'phone = %s, ' \
+                      'email = %s, ' \
+                      'date_of_birth = %s ' \
+                      'WHERE user_name = %s '
+                values = [f_name, l_name, phone_num, email_addr, u_dob, self.ids.nav_drawer_header.text, ]
+                self.mycursor.execute(sql, values)
+                self.mydb.commit()
+            except:
+                self.dialog = MDDialog(
+                    title="Error!",
+                    text="Cannot Update User",
+                    buttons=[
+                        MDFlatButton(
+                            text="Close",
+                            on_release=self.close_dialog
+                        )
+                    ]
+                )
+                self.dialog.open()
+            else:
+                self.dialog = MDDialog(
+                    title="Success!",
+                    text="User Info Updated Successfully!",
+                    buttons=[
+                        MDFlatButton(
+                            text="Close",
+                            on_release=self.close_dialog
+                        )
+                    ]
+                )
+                self.dialog.open()
+
+    def dob_date_picker(self):
+        self.dob_date = MDDatePicker(
+            min_year=1950,
+            max_year=2050,
+            primary_color=(0, 0, 0, 1),
+            selector_color=(0, 0, 0, 1),
+            text_button_color=(0, 0, 0, 1),
+            text_current_color=(0, 0, 0, 1),
+            input_field_text_color=(0, 0, 0, 1)
+        )
+        self.dob_date.bind(on_save=self.save_dob_date, on_cancel=self.close_dob_date_picker)
+        self.dob_date.open()
+
+    def save_dob_date(self, instance, value, date_range):
+        date = value.strftime("%d-%m-%Y")
+        self.ids.dob_fld.text = str(date)
+
+    def close_dob_date_picker(self, instance, value):
+        self.dob_date.dismiss(force=True)
 
     def goto_main_screen(self):
+        self.show_user_table()
         self.ids.scrn_mngr.transition.direction = "right"
         self.ids.scrn_mngr.current = "main_scrn"
         self.ids.toolbar.right_action_items = []
 
     def goto_edit_profile(self):
+        sql = 'SELECT first_name, last_name, phone, email, date_of_birth ' \
+              'FROM users WHERE user_name=%s'
+        values = [self.ids.nav_drawer_header.text, ]
+        self.mycursor.execute(sql, values)
+        result = self.mycursor.fetchone()
+        self.ids.first_name_fld.text = result[0] if result[0] else ""
+        self.ids.last_name_fld.text = result[1] if result[1] else ""
+        self.ids.phone_fld.text = result[2] if result[2] else ""
+        self.ids.email_fld.text = result[3] if result[3] else ""
+        self.ids.dob_fld.text = result[4] if result[4] else ""
         self.ids.scrn_mngr.transition.direction = "left"
         self.ids.scrn_mngr.current = "scrn_edit_profile"
 
     def goto_settings(self):
+        self.ids.old_pass.text = ""
+        self.ids.new_pass.text = ""
+        self.ids.confirm_pass.text = ""
         self.ids.scrn_mngr.transition.direction = "left"
         self.ids.scrn_mngr.current = "scrn_settings"
 
@@ -345,6 +502,7 @@ class AdminWindow(MDBoxLayout):
         self.ids.scrn_mngr.current = "scrn_update_trip"
 
     def goto_bus(self):
+        self.show_bus_table()
         if self.ids.scrn_mngr.current == "scrn_add_bus" or self.ids.scrn_mngr.current == "scrn_update_bus":
             self.ids.scrn_mngr.transition.direction = "right"
         else:
